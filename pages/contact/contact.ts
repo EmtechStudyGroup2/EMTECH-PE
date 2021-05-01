e3ce59f28d16661e1f80cc21018a57a8e4109785
EmtechStudyGroup2 <emtechstudygroup2@gmail.com>
2021-05-01 16:28:13 +0800

    4 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: "page-contact",
+  templateUrl: "contact.html"
 })
 export class ContactPage {
+  height: number;
+  weight: number;
+  calorie: number;
+  calorieMessage: string;
+  gender: string;
+  age: number;
+  activityLevel: number;
+  bmr: number;
+  carbohydrates: number;
+  proteins: number;
+  fats: number;
+  carboMessage: string;
+  proteinsMessage: string;
+  fatsMessage: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  calculateBMR() {
+    if ((this.gender = "m")) {
+      this.bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+    } else if ((this.gender = "f")) {
+      this.bmr = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
+    } else {
+      this.bmr = 0.01;
+    }
+    this.calorie = parseFloat((this.bmr * this.activityLevel).toFixed(2));
+    this.carbohydrates = parseFloat((0.6 * this.calorie).toFixed(0));
+    this.proteins = parseFloat((0.125 * this.calorie).toFixed(0));
+    this.fats = parseFloat((0.275 * this.calorie).toFixed(0));
   }
-
 }
